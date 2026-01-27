baab097ace152c92e4cd76905ae2c38e8d33f6e4
Scott Sharples <scott@blosm.dev>
2026-01-27 13:40:18 +0000

    fix: Use sync script tag for Tailwind CDN

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -15,7 +14,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script src="https://cdn.tailwindcss.com" strategy="beforeInteractive" />
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body className="bg-black text-white antialiased">{children}</body>
     </html>
